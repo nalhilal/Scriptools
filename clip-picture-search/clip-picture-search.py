0d8adc3a1596fcb2fd8cc75d79be65d7d403839c
nalhilal <nasser.alhilal@gmail.com>
2024-02-16 15:59:17 +0300

    added a method to check if image is suppored by Pillow (since later that's what will process the image)

--- clip-picture-search/clip-picture-search.py
+++ clip-picture-search/clip-picture-search.py
@@ -24,17 +24,41 @@ python image_recognition.py <image_folder> <output_file.csv>
 import argparse
 import os
 
+from pathlib import Path
+from PIL import Image
+
 
 def check_if_directory(directory_path):
+
     if not os.path.exists(directory_path):
         print(f"Directory not found: {directory_path}")
         exit(-1)
     elif not os.path.isdir(directory_path):
         print(f"Not a directory: {directory_path}")
         exit(-1)
+    else:
+        return True
+
+
+def find_image_files(directory):
+
+    image_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = Path(file)
+            if file_path.suffix.lower() in Image.registered_extensions():
+                image_files.append(os.path.join(root, file))
+    return image_files
+
+
+def main(directory):
+
+    image_files = find_image_files(directory)
+    print(image_files)
 
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "directory", type=str, help="Path to folder containing pictures to identify"
@@ -43,5 +67,6 @@ if __name__ == "__main__":
 
     if args.directory and check_if_directory(args.directory):
         directory = args.directory
+        main(directory)
     else:
         exit(-1)
